2faad1960ae0b4b5f55be124ff0094fe36fb8a38
Koblandy <kbaizhigit@outlook.com>
2019-08-26 22:39:09 +0600

    #11 chllng

---
+++ 11-areEquallyStrong/areEquallyStrong.ts
@@ -0,0 +1,19 @@
+function areEquallyStrong(
+  yourLeft: number,
+  yourRight: number,
+  friendsLeft: number,
+  friendsRight: number
+): boolean {
+  const yourWeakest = yourLeft <= yourRight ? yourLeft : yourRight;
+  const yourStrongest = yourLeft >= yourRight ? yourLeft : yourRight;
+  const friendsWeakest =
+    friendsLeft <= friendsRight ? friendsLeft : friendsRight;
+  const friendsStrongest =
+    friendsLeft >= friendsRight ? friendsLeft : friendsRight;
+
+  return yourWeakest === friendsWeakest && yourStrongest === friendsStrongest;
+}
+
+console.log(areEquallyStrong(10, 15, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 9));
